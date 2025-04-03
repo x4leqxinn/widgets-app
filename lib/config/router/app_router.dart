00cb891495518d5c3b6a0b71aff48e90930a5b93
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2025-04-03 01:54:41 -0300

    add(screens): new router added

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,11 +5,20 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
-    GoRoute(path: '/cards', builder: (context, state) => const CardsScreen()),
+    GoRoute(
+      path: '/cards',
+      name: CardsScreen.name,
+      builder: (context, state) => const CardsScreen(),
+    ),
   ],
 );
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CardsScreen extends StatelessWidget {
+  static const String name = 'card_screen';
+
   const CardsScreen({super.key});
 
   @override
